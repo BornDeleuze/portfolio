0ab50ece8f11a66fe2f2305cf151bc656cb09833
Matt Beaudoin <ehxtwo@gmail.com>
2025-05-20 14:01:38 -0700

    restyle and structure

--- app/router.js
+++ app/router.js
@@ -1,13 +1,14 @@
 import EmberRouter from '@ember/routing/router';
-import config from 'recipe-portfolio/config/environment';
+import config from './config/environment';
 
-export default class Router extends EmberRouter {
+const Router = class extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
-}
+};
 
 Router.map(function () {
-  this.route('home', { path: '/' });
   this.route('about');
-  this.route('contact');
+  this.route('recipe');
 });
+
+export default Router;
