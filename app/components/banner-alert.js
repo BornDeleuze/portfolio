0a6bdc68d730ee7885c4db404aa9380722143620
Matt Beaudoin <ehxtwo@gmail.com>
2025-07-02 03:56:09 -0700

    bottom CAL and animate alert

---
+++ app/components/banner-alert.js
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class BannerAlertComponent extends Component {
+  @tracked isHidden = false;
+
+  @action
+  dismiss() {
+    this.isHidden = true;
+
+    // Optional: remove from DOM after transition
+    setTimeout(() => {
+      let el = document.querySelector('.banner-alert');
+      if (el) {
+        el.style.display = 'none';
+      }
+    }, 400); // match your CSS transition duration
+  }
+}
